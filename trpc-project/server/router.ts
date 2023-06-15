b78d5810f3e8772a41d1a6e3b2bcbc54243308df
鈴木響 <h.suzuki@piton.jp>
2023-06-15 13:33:47 +0900

    改行

--- trpc-project/server/router.ts
+++ trpc-project/server/router.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 import { PrismaClient } from '@prisma/client';
 import { publicProcedure as procedure, router } from './trpc';
 const prisma = new PrismaClient();
-
 export const appRouter = router({
   hello: procedure
     .input(
